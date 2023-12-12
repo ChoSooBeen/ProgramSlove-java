10815cb5c86cc60f733d24d8525a33e3ffd37290
ChoSooBeen <cba0613060@gmail.com>
2023-12-12 14:20:20 +0900

    부분합 - two pointer

--- Baekjoon/TwoPointer1806.java
+++ Baekjoon/TwoPointer1806.java
@@ -13,7 +13,7 @@ public class TwoPointer1806 {
         int n = Integer.parseInt(st.nextToken());
         int s = Integer.parseInt(st.nextToken());
 
-        int[] nums = new int[n+1];
+        int[] nums = new int[n+1]; //n번째 값까지 확인할 수 있도록!
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i < n ; i++) {
             nums[i] = Integer.parseInt(st.nextToken());
@@ -22,12 +22,18 @@ public class TwoPointer1806 {
         System.out.println(minLength(nums, s));
     }
 
+    /**
+     * 부분합이 target 이상인 수열의 길이 중 최소 구하기
+     * @param nums 수열
+     * @param target 넘어야할 수
+     * @return 최솟값
+     */
     public static int minLength(int[] nums, int target) {
         int answer = Integer.MAX_VALUE;
 
         int start = 0;
         int end = 0;
-        int sum = 0;
+        int sum = 0; //start 인덱스 부터 end 인덱스 전까지의 합
         while(start < nums.length && end < nums.length) {
             if(sum >= target) {
                 answer = Math.min(answer, end - start);
