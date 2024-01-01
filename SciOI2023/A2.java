e8e2d45e3a42c8d05e8eca90d4fde59b166af77f
ChoSooBeen <cba0613060@gmail.com>
2024-01-01 17:21:10 +0900

    다오의 경주 대회 - Greedy

--- SciOI2023/A2.java
+++ SciOI2023/A2.java
@@ -1,2 +1,34 @@
-package SciOI2023;public class A2 {
+package SciOI2023;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class A2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] tracks = new int[n];
+        for(int i = 0; i < n; i++) {
+            tracks[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int count = 0;
+        for(int i = 1; i < n; i++) {
+            if(tracks[i-1] < tracks[i]) continue;
+            if(tracks[i-1] < tracks[i] + k) {
+                count++;
+                tracks[i] += k;
+            }
+            else {
+                count = -1;
+                break;
+            }
+        }
+        System.out.println(count);
+    }
 }
